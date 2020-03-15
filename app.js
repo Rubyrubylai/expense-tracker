5b810ccaacbcbd9eff96eb11444b2be85c2f4904
Ruby <r844312@gmail.com>
2020-03-16 00:07:52 +0800

    Add mongoose connection and create record model

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const ExpenseTracker = require('./models/record')
+
+mongoose.connect('mongodb://localhost/expenseTracker', {useNewUrlParser: true, useUnifiedTopology: true})
+
+const db = mongoose.connection
+
+db.on('error', ()=>{
+    console.log('mongodb error!')
+})
+
+db.once('open', ()=>{
+    console.log('mongodb connected!')
+})
+
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
