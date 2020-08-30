c2550d741ba187f0857b00f60f1298ed3f7048f0
Ruby Li <r844312@gmail.com>
2020-08-30 23:08:38 +0800

    Update Seeder to solve the bug that data cannot be stored into mongodb

--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -1,14 +1,16 @@
+const bcrypt = require('bcrypt')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const mongoose = require('mongoose')
 const Record = require('../record')
 const User = require('../user')
 const userSeed = require('./user.json')
 const recordSeed = require('./record.json')
-const bcrypt = require('bcrypt')
-if (process.env.NODE_ENV !== 'production') { 
-  require('dotenv').config()
-}
 
-mongoose.connect(process.env.MONGODB_URI ||' mongodb://localhost/expenseTracker', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
 const db = mongoose.connection
 
@@ -26,23 +28,32 @@ db.once('open', ()=>{
           name: user.name,
           email: user.email,
           password: hash
-        }).then(user => {
-          const record = index ? recordSeed.slice(2,5) : recordSeed.slice(0,2)
-          record.forEach(record => {
-            Record.create({
-              name: record.name,
-              category: record.category,
-              sort: record.sort,
-              date: record.date,
-              amount: record.amount,
-              userId: user._id,
-              merchant: record.merchant
-            })
-          })
-        }).catch(err => console.error(err))
+        })
+        .then(user => {     
+          return Promise.all(Array.from(
+            { length: 1 },
+            (_, i) => {
+                const record = index ? recordSeed.slice(2,5) : recordSeed.slice(0,2)
+                record.forEach(record => {
+                  Record.create({
+                    name: record.name,
+                    category: record.category,
+                    sort: record.sort,
+                    date: record.date,
+                    amount: record.amount,
+                    userId: user._id,
+                    merchant: record.merchant
+                  })                 
+                })                   
+            }
+          )) 
+        })
+        .then(() => {
+          console.log('done')  
+        })
       })
     })  
-  });
+  })
 
-  console.log('done')
+  
 })
